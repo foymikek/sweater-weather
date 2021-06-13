50aeb5b9ac7c64e270446e1c1210378be2ab48cc
foymikek <foymikek@gmail.com>
2021-06-12 20:40:21 -0600

    Add refactored attribute methods.

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -8,12 +8,27 @@ class DailyWeather
               :icon
 
   def initialize(data)
-    @date        = Time.at(data[:dt]).strftime("%m/%d/%Y at %I:%M%p")
-    @sunrise     = Time.at(data[:sunrise]).strftime("%m/%d/%Y at %I:%M%p")
-    @sunset      = Time.at(data[:sunset]).strftime("%m/%d/%Y at %I:%M%p")
+    @date        = date_attribute(data[:dt])
+    @sunrise     = sunrise_attribute(data[:sunrise])
+    @sunset      = sunset_attribute(data[:sunset])
     @max_temp    = data[:temp][:max]
     @min_temp    = data[:temp][:min]
     @conditions  = data[:weather][0][:description]
     @icon        = data[:weather][0][:icon]
+  end
+
+  def date_attribute(raw_time)
+    Time.at(raw_time)
+    .strftime("%m/%d/%Y at %I:%M%p")
+  end
+
+  def sunrise_attribute(raw_time)
+    Time.at(raw_time)
+    .strftime("%m/%d/%Y at %I:%M%p")
+  end
+
+  def sunset_attribute(raw_time)
+    Time.at(raw_time)
+    .strftime("%m/%d/%Y at %I:%M%p")
   end
 end

1ccccd590b95aa9e61ec8a41438f4f203d2d4e3c
foymikek <foymikek@gmail.com>
2021-06-13 11:46:27 -0600

    Add ForecastFacade.location_coords method refactor.

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -1,6 +1,33 @@
 class ForecastFacade
   class << self
     def forecast_query(location)
+      lat_n_lng = ForecastFacade.location_coords(location)      
+
+      conn = Faraday.new(url: 'http://api.openweathermap.org') do |f|
+        f.params['appid'] = ENV['weather_api_key']
+      end
+
+      response = conn.get('/data/2.5/onecall') do |req|
+          req.params['lat'] = lat_n_lng.lat
+          req.params['lon'] = lat_n_lng.lng
+          req.params['units'] = 'imperial'
+        end
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      
+      daily_weather = data[:daily][0..4].map do |daily_data|
+        DailyWeather.new(daily_data)
+      end
+
+      hourly_weather = data[:hourly][0..7].map do |hourly_data|
+        HourlyWeather.new(hourly_data)
+      end
+
+      json_input = Weather.new(CurrentWeather.new(data), daily_weather, hourly_weather)
+    end
+
+    def location_coords(location)
       conn = Faraday.new("http://www.mapquestapi.com") do |f|
           f.params['key'] = ENV['map_api_key']
         end
@@ -11,30 +38,7 @@ class ForecastFacade
       
       data = JSON.parse(response.body, symbolize_names: true)
       
-      lat_n_lng = LatNLng.new(data)
-      
-      conn = Faraday.new(url: 'http://api.openweathermap.org') do |f|
-        f.params['appid'] = ENV['weather_api_key']
-      end
-      
-      response = conn.get('/data/2.5/onecall') do |req|
-          req.params['lat'] = lat_n_lng.lat
-          req.params['lon'] = lat_n_lng.lng
-          req.params['units'] = 'imperial'
-        end
-      
-      data = JSON.parse(response.body, symbolize_names: true)
-      
-      
-      daily_weather = data[:daily][0..4].map do |daily_data|
-        DailyWeather.new(daily_data)
-      end
-      
-      hourly_weather = data[:hourly][0..7].map do |hourly_data|
-        HourlyWeather.new(hourly_data)
-      end
-      
-      json_input = Weather.new(CurrentWeather.new(data), daily_weather, hourly_weather)
+      LatNLng.new(data)
     end
   end
 end

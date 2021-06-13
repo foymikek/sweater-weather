77c4f28b4823bbec1389a6ef0ccb15284326b615
foymikek <foymikek@gmail.com>
2021-06-13 12:20:06 -0600

    Add refactored services.

--- app/services/map_quest_service.rb
+++ app/services/map_quest_service.rb
@@ -1,14 +1,22 @@
 class MapQuestService
   class << self
     def location_coords_db(location)
-      conn = Faraday.new("http://www.mapquestapi.com") do |f|
-          f.params['key'] = ENV['map_api_key']
-        end
-      
       response = conn.get("/geocoding/v1/address") do |r|
           r.params['location'] = location
         end
         
+      parse_json(response)
+    end
+
+    private
+
+    def conn
+      Faraday.new("http://www.mapquestapi.com") do |f|
+          f.params['key'] = ENV['map_api_key']
+      end
+    end
+
+    def parse_json(response)
       JSON.parse(response.body, symbolize_names: true)
     end
   end
--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -1,18 +1,13 @@
 class OpenWeatherService
   class << self
     def forecast_query_db(lat, lng)
-      conn = Faraday.new(url: 'http://api.openweathermap.org') do |f|
-        f.params['appid'] = ENV['weather_api_key']
-      end
-
       response = conn.get('/data/2.5/onecall') do |req|
           req.params['lat'] = lat
           req.params['lon'] = lng
           req.params['units'] = 'imperial'
-        end
+      end
 
-      data = JSON.parse(response.body, symbolize_names: true)
-
+      data = parse_json(response)
       
       daily_weather = data[:daily][0..4].map do |daily_data|
         DailyWeather.new(daily_data)
@@ -22,6 +17,18 @@ class OpenWeatherService
         HourlyWeather.new(hourly_data)
       end
       Weather.new(CurrentWeather.new(data), daily_weather, hourly_weather)
+    end
+
+    private
+
+    def conn
+      Faraday.new(url: 'http://api.openweathermap.org') do |f|
+        f.params['appid'] = ENV['weather_api_key']
+      end
+    end
+
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end
 end

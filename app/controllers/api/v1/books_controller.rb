ed8b3be9b86e8e1b8644c91c9a1bdf7fe72e9cee
foymikek <foymikek@gmail.com>
2021-06-14 09:37:45 -0600

    Add working but failing book request index test.

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::BooksController < ApplicationController
       r.params['location'] = location
     end
     data = JSON.parse(response.body, symbolize_names: true)
-    require 'pry'; binding.pry
+    # require 'pry'; binding.pry
+    # total_books_found: data[:num_found]
+    #isbn: data[:docs][0][:isbn]
+    # title: data[:docs][0][:title]
+    # publisher: data[:docs][0][:publisher]
   end
 end
--- spec/requests/book_search/index_spec.rb
+++ spec/requests/book_search/index_spec.rb
@@ -7,10 +7,48 @@ RSpec.describe "openbook API" do
       quantity = 5
       get "/api/v1/book-search?location=#{location}&quantity=#{quantity}"
 
+      # require 'pry'; binding.pry
       expect(response.status).to eq(200)
-      
       data     = JSON.parse(response.body, symbolize_names: true)
+      
       expect(data).to be_a(Hash)
+      expect(data).to have_key :data
+
+      expect(data[:data]).to have_key :id
+      expect(data[:data][:id]).to eq(nil)
+      expect(data[:data]).to have_key :type
+      expect(data[:data][:type]).to be_a(String)
+      expect(data[:data]).to have_key :attributes
+      expect(data[:data][:attributes]).to be_a(Hash)
+      
+      expect(data[:data][:attributes]).to have_key :destination
+      expect(data[:data][:attributes][:destination]).to be_a(String)
+      
+      expect(data[:data][:attributes]).to have_key :forecast
+      expect(data[:data][:attributes][:forecast]).to be_a(Hash)
+      expect(data[:data][:attributes][:forecast]).to have_key :summary
+      expect(data[:data][:attributes][:forecast][:sumary]).to be_a(String)
+      expect(data[:data][:attributes][:forecast]).to have_key :temperature
+      expect(data[:data][:attributes][:forecast][:temperature]).to be_a(String)
+      
+      expect(data[:data][:attributes]).to have_key :total_books_found
+      expect(data[:data][:attributes][:forecast][:total_books_found]).to be_a(Numeric)
+
+      expect(data[:data][:attributes]).to have_key :books
+      expect(data[:data][:attributes][:books]).to be_an(Array)
+      expect(data[:data][:attributes][:books].count).to eq(5)
+      expect(data[:data][:attributes][:books][0]).to be_a(Hash)
+      
+      expect(data[:data][:attributes][:books][0]).to have_key :isbn
+      expect(data[:data][:attributes][:books][0][:isbn]).to be_an(Array)
+      expect(data[:data][:attributes][:books][0][:isbn][0]).to be_a(String)
+      
+      expect(data[:data][:attributes][:books][0]).to have_key :title
+      expect(data[:data][:attributes][:books][0][:title]).to be_a(String)
+      
+      expect(data[:data][:attributes][:books][0]).to have_key :publisher
+      expect(data[:data][:attributes][:books][0][:publisher]).to be_a(String)
+
     end
   end
 end
